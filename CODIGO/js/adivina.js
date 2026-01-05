8836cc261752853a293b7b4dcd427d126eec788c
Carme Ferrer Alos <carmeferrer20@gmail.com>
2026-01-05 14:34:44 +0100

    Revisión Salas

--- CODIGO/js/adivina.js
+++ CODIGO/js/adivina.js
@@ -1,20 +1,3 @@
-/*    RATÓN    */
-
-document.addEventListener("mousemove", (e) => {
-    const glow = document.createElement("div");
-    glow.classList.add("cursor-glow");
-
-    glow.style.left = `${e.clientX - 6}px`;
-    glow.style.top = `${e.clientY - 6}px`;
-
-    document.body.appendChild(glow);
-
-    setTimeout(() => {
-        glow.remove();
-    }, 800);
-});
-
-
 /*    MINIJUEGO: ¿QUÉ PERSONAJE DE FICCIÓN ERES?    */
 
 let scores = {
@@ -97,3 +80,23 @@ function restart() {
     questionIndex = 0;
     location.reload();
 }
+
+/*    RATÓN    */
+/* Efecto visual que sigue el movimiento del ratón */
+document.addEventListener("mousemove", (e) => {
+    // Se crea un elemento visual para el efecto
+    const glow = document.createElement("div");
+    glow.classList.add("cursor-glow");
+
+    // Se posiciona el efecto en la posición del cursor
+    glow.style.left = `${e.clientX - 6}px`;
+    glow.style.top = `${e.clientY - 6}px`;
+
+    // Se añade al documento
+    document.body.appendChild(glow);
+
+    // Se elimina tras un breve tiempo para no saturar el DOM
+    setTimeout(() => {
+        glow.remove();
+    }, 800);
+});
--- CODIGO/js/contacto.js
+++ CODIGO/js/contacto.js
@@ -36,17 +36,21 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-  /*    RATÓN    */
-
+/*    RATÓN    */
+/* Efecto visual que sigue el movimiento del ratón */
 document.addEventListener("mousemove", (e) => {
+    // Se crea un elemento visual para el efecto
     const glow = document.createElement("div");
     glow.classList.add("cursor-glow");
 
+    // Se posiciona el efecto en la posición del cursor
     glow.style.left = `${e.clientX - 6}px`;
     glow.style.top = `${e.clientY - 6}px`;
 
+    // Se añade al documento
     document.body.appendChild(glow);
 
+    // Se elimina tras un breve tiempo para no saturar el DOM
     setTimeout(() => {
         glow.remove();
     }, 800);
